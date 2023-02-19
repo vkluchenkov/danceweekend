b64dafd7e8fdfc91c228751e2eb25d55562f608b
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-02-19 15:28:10 +0100

    feat: cover pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,21 @@ import styles from '@/styles/Home.module.css';
 import { Layout } from '@/src/components/Layout';
 import { useEffect } from 'react';
 import { Divider } from '@/src/components/Divider';
+import Image from 'next/image';
+import teachers from 'public/images/teachers.png';
 
 export default function Home() {
   return (
-    <>
-      <Layout>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia obcaecati, necessitatibus,
-          molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
-          minus eius deserunt non ullam!
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia obcaecati, necessitatibus,
-          molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
-          minus eius deserunt non ullam!
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia obcaecati, necessitatibus,
-          molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
-          minus eius deserunt non ullam!
-        </p>
-        <div className={styles.sticky}>
+    <Layout>
+      <div className={styles.coverContainer}>
+        <div className={styles.imageContainer}>
+          <Image src={teachers} alt='Dance weekend in Warsaw artists' fill />
+        </div>
+        <div className={styles.imageDivider}>
           <Divider />
         </div>
+      </div>
+      <section className={styles.content}>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia obcaecati, necessitatibus,
           molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
@@ -60,7 +52,7 @@ export default function Home() {
           molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
           minus eius deserunt non ullam!
         </p>
-      </Layout>
-    </>
+      </section>
+    </Layout>
   );
 }

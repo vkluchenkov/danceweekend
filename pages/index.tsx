ddc575f29cd4608ba43d0952ff94e3162b7d5516
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-02-19 12:20:39 +0100

    sticky problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import styles from '@/styles/Home.module.css';
 import { Layout } from '@/src/components/Layout';
+import { useEffect } from 'react';
+import { Divider } from '@/src/components/Divider';
 
 export default function Home() {
   return (
@@ -20,6 +22,9 @@ export default function Home() {
           molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
           minus eius deserunt non ullam!
         </p>
+        <div className={styles.sticky}>
+          <Divider />
+        </div>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia obcaecati, necessitatibus,
           molestias aperiam sunt, ab ipsa ex nisi voluptatibus explicabo facilis nulla dolor sint et
---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,5 @@
+import styles from './divider.module.css';
+
+export const Divider: React.FC = () => {
+  return <div className={styles.divider}></div>;
+};

13df66e41a32d1bee7f8792a7a5cdbd2660b90d4
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-03-01 21:43:32 +0100

    fix: accordeon styling

--- src/ui-kit/StyledAccordeon/index.tsx
+++ src/ui-kit/StyledAccordeon/index.tsx
@@ -3,7 +3,8 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import { StyledEngineProvider } from '@mui/material/styles';
 import styles from './StyledAccordeon.module.css';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import clsx from 'clsx';
 
 interface StyledAccordeonProps {
   summary: string;
@@ -11,9 +12,15 @@ interface StyledAccordeonProps {
 }
 
 export const StyledAccordeon: React.FC<StyledAccordeonProps> = ({ summary, details }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <StyledEngineProvider injectFirst>
-      <Accordion disableGutters className={styles.accordeon}>
+      <Accordion
+        disableGutters
+        className={clsx(styles.accordeon, isExpanded && styles.accordeon_expanded)}
+        onChange={(event, expanded) => setIsExpanded(expanded)}
+      >
         <AccordionSummary className={styles.summary}>{summary}</AccordionSummary>
         <AccordionDetails>{details}</AccordionDetails>
       </Accordion>

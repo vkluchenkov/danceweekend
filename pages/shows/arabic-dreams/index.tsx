fb658697961944215ed08fc99c991cfc25509758
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-03-03 16:03:55 +0100

    feat: arabic dreams page

--- pages/shows/arabic-dreams/index.tsx
+++ pages/shows/arabic-dreams/index.tsx
@@ -5,11 +5,34 @@ import styles from '@/styles/ArabicDreams.module.css';
 import useTranslation from 'next-translate/useTranslation';
 import Trans from 'next-translate/Trans';
 import Link from 'next/link';
+import arabicDreamsLogo from '/public/images/arabicDreams_logo.png';
+import Image from 'next/image';
 
 const ArabicDreams: NextPage = () => {
   const { t, lang } = useTranslation('arabicDreams');
 
-  const content = <></>;
+  const content = (
+    <>
+      <div className={styles.logoWrapper}>
+        <Image src={arabicDreamsLogo} alt={t('pageTitle')} fill />
+      </div>
+      <p className={textStyles.p}>
+        <span className={textStyles.accent}>{t('dateTitle')}:</span> 18.08.2023 / 20:00
+      </p>
+      <p className={textStyles.p}>{t('description')}</p>
+      <p className={textStyles.p}>{t('videoDescription')}</p>
+      <div className={styles.playerWrapper}>
+        <iframe
+          src='https://player.vimeo.com/video/743089078?h=27a33d066b'
+          width='100%'
+          height='100%'
+          frameBorder='0'
+          allow='autoplay; fullscreen'
+          allowFullScreen
+        />
+      </div>
+    </>
+  );
 
   return (
     <Layout title={t('pageTitle')}>

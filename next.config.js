eb898f5dfa9169a4c747c35292431d8080219d24
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-03-17 23:14:09 +0100

    feat: image optimizations

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextTranslate = require('next-translate-plugin');
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  images: {
+    formats: ['image/webp'],
+  },
 };
 
 module.exports = nextTranslate(nextConfig);

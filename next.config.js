c0618e190a5a1894c21872b7667d83132292968f
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-03-17 23:38:11 +0100

    fix: reverse image optimization

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextTranslate = require('next-translate-plugin');
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  images: {
-    formats: ['image/webp'],
-  },
 };
 
 module.exports = nextTranslate(nextConfig);

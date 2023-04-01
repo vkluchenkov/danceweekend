c0e12a11d54698df5a588692ad22ac509075c60e
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-04-01 11:54:18 +0200

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import { montserrat } from '@/src/ulis/font';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   const paypalClientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!;
 
   return (
     <>
+      <GoogleAnalytics trackPageViews strategy='lazyOnload' />
       <PayPalScriptProvider options={{ 'client-id': paypalClientId, currency: 'EUR' }}>
         <style jsx global>{`
           html {

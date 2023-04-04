b00b32fb6a8100a0da31368839119242d6a7cdf3
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-04-04 11:22:37 +0200

    feat: notion live

--- src/notion/saveRegistrationToNotion.ts
+++ src/notion/saveRegistrationToNotion.ts
@@ -2,6 +2,7 @@ import { Client } from '@notionhq/client';
 import { OrderPayload } from '../components/FormLive/types';
 import { contestCategories } from '../ulis/contestCategories';
 import { Translate } from 'next-translate';
+import { CreatePageParameters } from '@notionhq/client/build/src/api-endpoints';
 
 interface saveRegistrationToNotionProps {
   form: OrderPayload;
@@ -75,162 +76,166 @@ export const saveRegistrationToNotion = async (props: saveRegistrationToNotionPr
     ? form.worldShowGroup?.name + ' | ' + form.worldShowGroup?.qty + ' pers.'
     : '';
 
-  try {
-    await notion.pages.create({
-      parent: {
-        database_id: process.env.NOTION_DATABASE_LIVE!,
-        type: 'database_id',
+  const livePayload: CreatePageParameters = {
+    parent: {
+      database_id: process.env.NOTION_DATABASE_LIVE!,
+      type: 'database_id',
+    },
+    properties: {
+      Name: {
+        type: 'title',
+        title: [
+          {
+            type: 'text',
+            text: { content: form.name },
+          },
+        ],
       },
-      properties: {
-        Name: {
-          type: 'title',
-          title: [
-            {
-              type: 'text',
-              text: { content: form.name },
+      Surname: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.surname },
+          },
+        ],
+      },
+      'Stage name': {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.stageName },
+          },
+        ],
+      },
+      Age: {
+        type: 'number',
+        number: Number.parseInt(form.age as unknown as string),
+      },
+      Social: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.social },
+          },
+        ],
+      },
+      Country: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.country },
+          },
+        ],
+      },
+      City: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.city },
+          },
+        ],
+      },
+      Phone: {
+        type: 'phone_number',
+        phone_number: form.tel,
+      },
+      Email: {
+        type: 'email',
+        email: form.email,
+      },
+      FP: {
+        type: 'checkbox',
+        checkbox: form.isFullPass,
+      },
+      'Full Pass discount': {
+        type: 'select',
+        select: fullPassDiscountSelect() ? { name: fullPassDiscountSelect()! } : null,
+      },
+      'Group name': {
+        type: 'select',
+        select: form.fullPassGroupName ? { name: form.fullPassGroupName } : null,
+      },
+      'Certificate source': {
+        type: 'rich_text',
+        rich_text: [
+          {
+            text: {
+              content: form.fullPassDiscountSource || '',
             },
-          ],
-        },
-        Surname: {
-          type: 'rich_text',
-          rich_text: [
-            {
-              type: 'text',
-              text: { content: form.surname },
-            },
-          ],
-        },
-        'Stage name': {
-          type: 'rich_text',
-          rich_text: [
-            {
-              type: 'text',
-              text: { content: form.stageName },
-            },
-          ],
-        },
-        Age: {
-          type: 'number',
-          number: Number.parseInt(form.age as unknown as string),
-        },
-        Social: {
-          type: 'rich_text',
-          rich_text: [
-            {
-              type: 'text',
-              text: { content: form.social },
-            },
-          ],
-        },
-        Country: {
-          type: 'rich_text',
-          rich_text: [
-            {
-              type: 'text',
-              text: { content: form.country },
-            },
-          ],
-        },
-        City: {
-          type: 'rich_text',
-          rich_text: [
-            {
-              type: 'text',
-              text: { content: form.city },
-            },
-          ],
-        },
-        Phone: {
-          type: 'phone_number',
-          phone_number: form.tel,
-        },
-        Email: {
-          type: 'email',
-          email: form.email,
-        },
-        FP: {
-          type: 'checkbox',
-          checkbox: form.isFullPass,
-        },
-        'Full Pass discount': {
-          type: 'select',
-          select: fullPassDiscountSelect() ? { name: fullPassDiscountSelect()! } : null,
-        },
-        'Group name': {
-          type: 'select',
-          select: form.fullPassGroupName ? { name: form.fullPassGroupName } : null,
-        },
-        'Certificate source': {
-          type: 'rich_text',
-          rich_text: [
-            {
-              text: {
-                content: form.fullPassDiscountSource || '',
-              },
-            },
-          ],
-        },
-        'Single WS': {
-          type: 'multi_select',
-          multi_select: singleWs,
-        },
-        'Solo Contest': {
-          type: 'checkbox',
-          checkbox: !!form.isSoloContest,
-        },
-        'Age group': {
-          type: 'select',
-          select: form.isSoloContest
-            ? { name: t(`form.contest.ageGroups.${form.contestAgeGroup}`)! }
+          },
+        ],
+      },
+      'Single WS': {
+        type: 'multi_select',
+        multi_select: singleWs,
+      },
+      'Solo Contest': {
+        type: 'checkbox',
+        checkbox: !!form.isSoloContest,
+      },
+      'Age group': {
+        type: 'select',
+        select: form.isSoloContest
+          ? { name: t(`form.contest.ageGroups.${form.contestAgeGroup}`)! }
+          : null,
+      },
+      Level: {
+        type: 'select',
+        select:
+          form.isSoloContest && form.contestLevel
+            ? { name: t(`form.contest.levels.${form.contestLevel}`)! }
             : null,
-        },
-        Level: {
-          type: 'select',
-          select:
-            form.isSoloContest && form.contestLevel
-              ? { name: t(`form.contest.levels.${form.contestLevel}`)! }
-              : null,
-        },
-        'Solo Pass': {
-          type: 'checkbox',
-          checkbox: !!form.isSoloPass,
-        },
-        Styles: {
-          type: 'multi_select',
-          multi_select: contestSoloStyles!,
-        },
-        'Group / Duo contest': {
-          type: 'checkbox',
-          checkbox: !!form.isGroupContest,
-        },
-        'Groups/Duos': {
-          type: 'multi_select',
-          multi_select: contestGroups,
-        },
-        'World Show solo': {
-          type: 'checkbox',
-          checkbox: !!form.isWorldShowSolo,
-        },
-        'World Show group': {
-          type: 'rich_text',
-          rich_text: [
-            {
-              text: {
-                content: worldShowGroup,
-              },
-            },
-          ],
-        },
-        Installments: {
-          type: 'checkbox',
-          checkbox: !!form.isInstallments,
-        },
-        Total: {
-          type: 'number',
-          number: form.total,
-        },
       },
-    });
+      'Solo Pass': {
+        type: 'checkbox',
+        checkbox: !!form.isSoloPass,
+      },
+      Styles: {
+        type: 'multi_select',
+        multi_select: contestSoloStyles!,
+      },
+      'Group / Duo contest': {
+        type: 'checkbox',
+        checkbox: !!form.isGroupContest,
+      },
+      'Groups/Duos': {
+        type: 'multi_select',
+        multi_select: contestGroups,
+      },
+      'World Show solo': {
+        type: 'checkbox',
+        checkbox: !!form.isWorldShowSolo,
+      },
+      'World Show group': {
+        type: 'rich_text',
+        rich_text: [
+          {
+            text: {
+              content: worldShowGroup,
+            },
+          },
+        ],
+      },
+      Installments: {
+        type: 'checkbox',
+        checkbox: !!form.isInstallments,
+      },
+      Total: {
+        type: 'number',
+        number: form.total,
+      },
+    },
+  };
+
+  const notionPayload = livePayload;
+
+  try {
+    await notion.pages.create(notionPayload);
   } catch (error) {
     console.log(error);
   }

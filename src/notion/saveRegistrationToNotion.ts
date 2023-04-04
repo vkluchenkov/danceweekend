4b108f7c43e07faa184a940e6b9372502b2593f2
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-04-04 11:34:14 +0200

    feat: notion online

--- src/notion/saveRegistrationToNotion.ts
+++ src/notion/saveRegistrationToNotion.ts
@@ -232,7 +232,137 @@ export const saveRegistrationToNotion = async (props: saveRegistrationToNotionPr
     },
   };
 
-  const notionPayload = livePayload;
+  const onlinePayload: CreatePageParameters = {
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ONLINE!,
+      type: 'database_id',
+    },
+    properties: {
+      Name: {
+        type: 'title',
+        title: [
+          {
+            type: 'text',
+            text: { content: form.name },
+          },
+        ],
+      },
+      Surname: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.surname },
+          },
+        ],
+      },
+      'Stage name': {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.stageName },
+          },
+        ],
+      },
+      Age: {
+        type: 'number',
+        number: Number.parseInt(form.age as unknown as string),
+      },
+      Social: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.social },
+          },
+        ],
+      },
+      Country: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.country },
+          },
+        ],
+      },
+      City: {
+        type: 'rich_text',
+        rich_text: [
+          {
+            type: 'text',
+            text: { content: form.city },
+          },
+        ],
+      },
+      Phone: {
+        type: 'phone_number',
+        phone_number: form.tel,
+      },
+      Email: {
+        type: 'email',
+        email: form.email,
+      },
+      FP: {
+        type: 'checkbox',
+        checkbox: form.isFullPass,
+      },
+      'Full Pass discount': {
+        type: 'select',
+        select: fullPassDiscountSelect() ? { name: fullPassDiscountSelect()! } : null,
+      },
+      'Certificate source': {
+        type: 'rich_text',
+        rich_text: [
+          {
+            text: {
+              content: form.fullPassDiscountSource || '',
+            },
+          },
+        ],
+      },
+      'Single WS': {
+        type: 'multi_select',
+        multi_select: singleWs,
+      },
+      'Solo Contest': {
+        type: 'checkbox',
+        checkbox: !!form.isSoloContest,
+      },
+      'Age group': {
+        type: 'select',
+        select: form.isSoloContest
+          ? { name: t(`form.contest.ageGroups.${form.contestAgeGroup}`)! }
+          : null,
+      },
+      Level: {
+        type: 'select',
+        select:
+          form.isSoloContest && form.contestLevel
+            ? { name: t(`form.contest.levels.${form.contestLevel}`)! }
+            : null,
+      },
+      Styles: {
+        type: 'multi_select',
+        multi_select: contestSoloStyles!,
+      },
+      'Group / Duo contest': {
+        type: 'checkbox',
+        checkbox: !!form.isGroupContest,
+      },
+      'Groups/Duos': {
+        type: 'multi_select',
+        multi_select: contestGroups,
+      },
+      Total: {
+        type: 'number',
+        number: form.total,
+      },
+    },
+  };
+
+  const notionPayload = form.version === 'live' ? livePayload : onlinePayload;
 
   try {
     await notion.pages.create(notionPayload);

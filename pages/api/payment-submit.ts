8a15cd7ea01ffcd07d3e22795b606e40dd389b9a
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2024-03-14 21:35:12 +0100

    Refactor payment-submit API endpoint to validate form fields before sending email

--- pages/api/payment-submit.ts
+++ pages/api/payment-submit.ts
@@ -8,20 +8,26 @@ import { sendMail } from '@/src/email/sendMail';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const orderPayload: PaymentFormFields = req.body;
   const t = await getT('en', 'payment');
+  const { name, email, qty, method } = orderPayload;
 
-  const adminEmailContent = paymentAdminEmail({ form: orderPayload, t: t }).html;
-  const adminEmailErrors = paymentAdminEmail({ form: orderPayload, t: t }).errors;
+  if (!name || !email || !qty || !method) {
+    res.status(400).send({ message: 'Bad request' });
+    return;
+  } else {
+    const adminEmailContent = paymentAdminEmail({ form: orderPayload, t: t }).html;
+    const adminEmailErrors = paymentAdminEmail({ form: orderPayload, t: t }).errors;
 
-  const adminMailPayload = {
-    senderEmail: senderEmail,
-    senderName: senderName,
-    recipientEmail: senderEmail,
-    recipientName: senderName,
-    recipientSubj: t('email.title') + ' ' + orderPayload.name,
-    mailContent: adminEmailContent,
-  };
+    const adminMailPayload = {
+      senderEmail: senderEmail,
+      senderName: senderName,
+      recipientEmail: senderEmail,
+      recipientName: senderName,
+      recipientSubj: t('email.title') + ' ' + orderPayload.name,
+      mailContent: adminEmailContent,
+    };
 
-  sendMail(adminMailPayload);
-
-  res.status(200).send({ message: 'Ok' });
+    sendMail(adminMailPayload);
+    res.status(200).send({ message: 'Ok' });
+    return;
+  }
 }
